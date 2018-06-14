e010059909e71358c1ea297cd29c363bc1e6a031
Yodae Lim <yodae.lim@gmail.com>
2018-06-13 23:06:11 -0700

    analyze books and analyze sentences

--- sentiment_analysis/analyze_books.R
+++ sentiment_analysis/analyze_books.R
@@ -1,6 +1,6 @@
 # install.packages('dplyr')
 # install.packages('stringr')
-# install.packages('tidytext')
+#install.packages('tidytext')
 # install.packages('tidyr')
 # install.packages('ggplot2')
 
@@ -15,25 +15,26 @@ library(ggplot2)
 # and negative words. Use the lexicon which categorizes words into
 # positive and negative.
 
-
-
+get_sentiments("nrc")
+get_sentiments("afin")
+bing_sentiments <- get_sentiments("bing")
 
 ##### DATA ANALYSIS + WRANGLING #####
 # Read books data in 
-
-
-
-
+books <- read.csv('data/austen_books.csv', stringsAsFactors = FALSE)
 
 # Map each word in the 'books' dataset to its dictionary-prescribed sentiment.
-
+jane_austen_sentiment <- books %>%
+  inner_join(bing_sentiments, by = "word")
+  
 
 
 
 
 # Instead of having each individual word, count the number of positive/negative
 # words in each chapter.
-
+jane_austen_sentiment <- jane_austen_sentiment %>%
+  count(book, chapter, sentiment)
 
 
 
@@ -41,14 +42,18 @@ library(ggplot2)
 # A chapter's overarching feeling will be calculated by the number of positive
 # words minus the number of negative words. Create a new column called 
 # 'sentiment' with this value.
-
+jane_austen_sentiment <- jane_austen_sentiment %>%
+  spread(sentiment, n, fill = 0) %>%
+  mutate(sentiment = positive - negative)
 
 
 
 
 ##### CREATE OUR VISUALIZATION #####
 # Use ggplot to plot each chapter's sentiment by book.
-
+ggplot(jane_austen_sentiment, aes(chapter, sentiment, fill = book)) +
+  geom_col(show.legend = FALSE)+
+  facet_wrap(~book, ncol = 2, scales = "free_x")
 
 
 
